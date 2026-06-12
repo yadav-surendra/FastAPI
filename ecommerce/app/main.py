0f485c3d51fa2c2d584c52f4fcfa9f1bf39367f2
yadav-surendra <ysurendraaprasad2007@gmail.com>
2026-06-12 21:48:37 +0545

    limit and offset added

--- ecommerce/app/main.py
+++ ecommerce/app/main.py
@@ -23,7 +23,31 @@ def list_products(
         min_length=1,
         max_length=50,
         description="Filter products by name"
-        )
+        ),
+    
+
+    sort_by_price: bool = Query(
+        default=False,
+        description="Sort products by price"
+    ),
+
+    order: str = Query(
+        default="asc",
+        description="Sort order: 'asc' for ascending, 'desc' for descending"
+    ),
+
+    limit: int = Query(
+        default=10,
+        ge=1,
+        le=100,
+        description="Limit the number of products returned"
+    ),
+    offset: int = Query(
+        default=0,
+        ge=0,
+        description="Number of products to skip before starting to collect the result set" 
+    )
+
     ):
     
     products = get_all_products()
@@ -32,14 +56,23 @@ def list_products(
         needle = name.strip().lower()
         products = [p for p in products if needle in p.get("name","").lower()]
 
-        if not products:
-            raise HTTPException(
-                status_code=404, detail=f"No products found matching the name={name}"
-                )
-        
+    if not products:
+        raise HTTPException(
+            status_code=404, detail=f"No products found matching the name={name}"
+            )
+    
+    if sort_by_price:
+        reverse = order == "desc"
+        products = sorted(products, key=lambda p: p.get("price", 0), reverse=reverse)
+        # if reverse is true, it will sort in descending order, otherwise in ascending
+
+
     total = len(products)   
+    products = products[offset:offset+limit]  # Apply the limit to the number of products returned
 
     return {
         "total": total,
         "items": products
         }
+
+## working upto here
